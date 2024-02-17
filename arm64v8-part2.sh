0d58f60d2875f8eee7fd790658df8ccc3c7524ef
zcmi <2319888757@qq.com>
2024-02-17 23:09:38 +0800

    Update arm64v8-part2.sh

--- arm64v8-part2.sh
+++ arm64v8-part2.sh
@@ -3,14 +3,18 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-sed -i 's/TARGET_CFLAGS += -DHAVE_MAP_SYNC/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/' feeds/packages/utils/xfsprogs/Makefile
+
+#git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+
+#sed -i 's/TARGET_CFLAGS += -DHAVE_MAP_SYNC/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/' feeds/packages/utils/xfsprogs/Makefile
 
 # ==========openclash===========
-cd package
-git clone --depth=1 --filter=blob:none --sparse https://github.com/vernesong/OpenClash.git
-cd OpenClash
-git sparse-checkout set luci-app-openclash
-cd ../..
+#cd package
+#git clone --depth=1 --filter=blob:none --sparse https://github.com/vernesong/OpenClash.git
+#cd OpenClash
+#git sparse-checkout set luci-app-openclash
+#cd ../..
 
