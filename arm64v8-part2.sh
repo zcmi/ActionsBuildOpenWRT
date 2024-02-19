1bca7d57d22b729eacbd7241ec2a2a17a3f37504
zcmi <2319888757@qq.com>
2024-02-19 16:20:12 +0800

    Update arm64v8-part2.sh

--- arm64v8-part2.sh
+++ arm64v8-part2.sh
@@ -3,8 +3,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 
 #git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
